ee28f9cb1842b439d255a73681bdcca67a00aad5
rbpartC <ruben.partouche@calibsun.com>
2025-09-26 07:42:55 +0200

    fix: dockerfile and cleanup

--- temporal-workflows/client.py
+++
@@ -1,7 +0,0 @@
-import os 
-from temporalio.client import Client
-
-TEMPORAL_HOST = f"{os.getenv("TEMPORAL_HOST", "localhost")}:{os.getenv("TEMPORAL_PORT", "7233")}"
-
-async def get_client() -> Client:
-    return await Client.connect(TEMPORAL_HOST)
