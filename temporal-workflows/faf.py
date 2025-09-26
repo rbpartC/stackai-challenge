24b5951ea735c3327efe55be81b989148bda5785
rbpartC <ruben.partouche@calibsun.com>
2025-09-26 11:26:48 +0200

    fix: input validation

--- temporal-workflows/faf.py
+++ temporal-workflows/faf.py
@@ -19,10 +19,11 @@ class ForgettableWorkflow:
 @workflow.defn
 class FiringWorkflow:
     @workflow.run
-    async def run(self, param: PositiveInt) -> str:
+    async def run(self, param: int) -> str:
+        validated = PositiveInt(value=param)
         wf = await workflow.start_child_workflow(
             ForgettableWorkflow.run,
-            param,
+            validated,
             task_queue=settings.EXAMPLE_SYNC_QUEUE,
         )
         return f"Fired forgettable workflow with run_id {wf.id} without waiting. The ForgettableWorkflow will run in the background for {param} seconds."
