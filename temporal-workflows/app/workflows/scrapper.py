b66b4bf2a563d90793bb0a2f297b003786691372
rbpartC <ruben.partouche@calibsun.com>
2025-09-28 12:32:51 +0200

    feat: scrapper worklow + update tests

--- temporal-workflows/app/workflows/scrapper.py
+++ temporal-workflows/app/workflows/scrapper.py
@@ -13,23 +13,13 @@ with workflow.unsafe.imports_passed_through():
     import requests
     from bs4 import BeautifulSoup
 
-    def gen_year_month(up_to: str) -> list[str]:
-        up_to_date = datetime.strptime(up_to, DATEFORMAT)
-        current_date = datetime.now()
-        days = [
-            up_to_date + timedelta(days=date)
-            for date in range(0, (current_date - up_to_date).days + 1)
-        ]
-        year_months = list(set([date.strftime("%Y/%m") for date in days]))
-        year_months.sort()
-        return year_months
-
 
 class ScrapParams(BaseModel):
     tag: str = Field(min_length=1)
     # Validate go_back_to and current_date as YYYY-MM if not None
     go_back_to: Optional[str] = None
-    current_date_index: Optional[int] = None
+    current_date_index: int = 0
+    archives_dates: Optional[list[str]] = None
     urls: list[str] = []
 
     @pydantic.field_validator("go_back_to")
@@ -42,11 +32,29 @@ class ScrapParams(BaseModel):
         return v
 
 
+class GetLinksParams(BaseModel):
+    tag: str = Field(min_length=1)
+    archive_date: str = Field(min_length=7, max_length=7)  # YYYY-MM format
+
+
+@activity.defn
+async def gen_year_month(up_to: str) -> list[str]:
+    up_to_date = datetime.strptime(up_to, DATEFORMAT)
+    current_date = datetime.now()
+    days = [
+        up_to_date + timedelta(days=date)
+        for date in range(0, (current_date - up_to_date).days + 1)
+    ]
+    year_months = list(set([date.strftime("%Y/%m") for date in days]))
+    year_months.sort()
+    return year_months
+
+
 # Activity to process a chunk of data with heartbeat progress reporting
 @activity.defn
-async def get_links(tag: str, archive_date: str = "") -> list[str]:
-    url = f"{BASE}/tag/{tag}/archive"
-    urls = ["/".join([url, s]) for s in archive_date]
+async def get_links(params: GetLinksParams) -> list[str]:
+    url = f"{BASE}/tag/{params.tag}/archive"
+    urls = ["/".join([url, s]) for s in params.archive_date]
     links = []
     for url in urls:
         data = requests.get(url)
@@ -70,27 +78,34 @@ class ExtractLinksWorkflow:
         # If all data processed, return total
         urls = []
 
-        if scrap_params.go_back_to is None:
+        if scrap_params.go_back_to is None and scrap_params.archives_dates is None:
             archive_dates = [datetime.now().strftime(DATEFORMAT)]
         else:
-            archive_dates = gen_year_month(scrap_params.go_back_to)
+            archive_dates = await workflow.execute_activity(
+                gen_year_month,
+                scrap_params.go_back_to,
+                start_to_close_timeout=timedelta(seconds=30),
+            )
+
+        urls = await workflow.execute_activity(
+            get_links,
+            GetLinksParams(
+                tag=scrap_params.tag,
+                archive_date=archive_dates[scrap_params.current_date_index],
+            ),
+            start_to_close_timeout=timedelta(seconds=30),
+        )
 
         if len(archive_dates) == scrap_params.current_date_index + 1:
             return urls
-
-        if scrap_params.current_date_index is not None:
-            urls = await workflow.execute_activity(
-                get_links,
-                scrap_params.tag,
-                archive_date=archive_dates[scrap_params.current_date_index],
-            )
         # Continue as new for next chunk
         return await workflow.continue_as_new(
             args=[
                 ScrapParams(
                     tag=scrap_params.tag,
+                    archives_dates=archive_dates,
                     go_back_to=scrap_params.go_back_to,
-                    current_date_index=scrap_params.current_date_index + 1 or 0,
+                    current_date_index=scrap_params.current_date_index + 1,
                     urls=scrap_params.urls + urls,
                 )
             ],
@@ -98,4 +113,4 @@ class ExtractLinksWorkflow:
 
 
 scrapper_workflows = [ExtractLinksWorkflow]
-scrapper_activities = [get_links]
+scrapper_activities = [get_links, gen_year_month]
--- temporal-workflows/tests/test_scrap.py
+++ temporal-workflows/tests/test_scrap.py
@@ -1,35 +1,40 @@
 from datetime import datetime
 from unittest.mock import patch
 
+import pytest
 from workflows.scrapper import gen_year_month
 
-fixed_now = datetime.datetime(2025, 9, 1)
+fixed_now = datetime(2025, 9, 1)
 
 
 @patch("datetime.datetime")
-def test_gen_list_1(mock_datetime):
+@pytest.mark.asyncio
+async def test_gen_list_1(mock_datetime):
     mock_datetime.now.return_value = fixed_now
     mock_datetime.side_effect = lambda *args, **kwargs: datetime(*args, **kwargs)
-    assert gen_year_month("2025-09") == [
-        "2025/09"
-    ], "Should return only the current month"
+    res = await gen_year_month("2025-09")
+    assert res == ["2025/09"], "Should return only the current month"
 
 
 @patch("datetime.datetime")
-def test_gen_list_2(mock_datetime):
+@pytest.mark.asyncio
+async def test_gen_list_2(mock_datetime):
     mock_datetime.now.return_value = fixed_now
     mock_datetime.side_effect = lambda *args, **kwargs: datetime(*args, **kwargs)
-    assert gen_year_month("2025-08") == [
+    res = await gen_year_month("2025-08")
+    assert res == [
         "2025/08",
         "2025/09",
     ], "Should return the current and next month"
 
 
 @patch("datetime.datetime")
-def test_gen_list_3(mock_datetime):
+@pytest.mark.asyncio
+async def test_gen_list_3(mock_datetime):
     mock_datetime.now.return_value = fixed_now
     mock_datetime.side_effect = lambda *args, **kwargs: datetime(*args, **kwargs)
-    assert gen_year_month("2025-02") == [
+    res = await gen_year_month("2025-02")
+    assert res == [
         "2025/02",
         "2025/03",
         "2025/04",
